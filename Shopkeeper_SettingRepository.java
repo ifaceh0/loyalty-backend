4af8def1f40295876fedd1eb2a0da591fa902989
Sumankumar312 <sumankumarsahoo312@gmail.com>
2025-04-30 21:45:43 +0530

    Add files via upload

---
+++ Shopkeeper_SettingRepository.java
@@ -0,0 +1,9 @@
+package com.ss.Loyalty.Project.Repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.ss.Loyalty.Project.Entity.Shopkeeper_Setting;
+
+public interface Shopkeeper_SettingRepository extends JpaRepository<Shopkeeper_Setting, Long> {
+
+}
