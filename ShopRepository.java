42479e81ce35b031c066bc4e214152172f3765f5
Sachin Kumar <156211576+SachinKumar9009@users.noreply.github.com>
2025-05-06 00:03:45 +0530

    Delete ShopRepository.java

--- ShopRepository.java
+++
@@ -1,9 +0,0 @@
-package com.sts.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.sts.entity.Shop;
-
-public interface ShopRepository extends JpaRepository<Shop, Long>{
-
-}
