6831c3d4232ff00f92f2dc94aeddf118c67ffb0b
Lappy <Lappy@HPPROBOOK.lan>
2025-05-06 00:53:46 +0530

    Docker file added

---
+++ Loyalty_Program/src/main/java/com/sts/repository/ShopRepository.java
@@ -0,0 +1,9 @@
+package com.sts.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.sts.entity.Shop;
+
+public interface ShopRepository extends JpaRepository<Shop, Long>{
+
+}
