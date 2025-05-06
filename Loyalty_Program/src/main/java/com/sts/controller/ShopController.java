d3ac0235e0938f7dd865cd5a8f465f0aeec6a3f1
Lappy <Lappy@HPPROBOOK.lan>
2025-05-06 20:25:59 +0530

    controller classes added

--- Loyalty_Program/src/main/java/com/sts/controller/ShopController.java
+++ Loyalty_Program/src/main/java/com/sts/controller/ShopController.java
@@ -1,17 +1,26 @@
 package com.sts.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.sts.entity.Shop;
-import com.sts.repository.ShopRepository;
+import com.sts.service.ShopService;
+import org.springframework.web.bind.annotation.PostMapping;
 
-@RestController
+
+@RestController("shop")
 public class ShopController {
 	@Autowired
-	private ShopRepository shopRepository;
+	private ShopService shopService;
 	
+	@PostMapping("saveShop")
+	public ResponseEntity<Shop> createShop(@RequestBody Shop shop) {
+		Shop shopDetails = shopService.createShop(shop);
+		return new ResponseEntity<Shop>(shopDetails, HttpStatus.CREATED);
+	}
 	
 	
 }
