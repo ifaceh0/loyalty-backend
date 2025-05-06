3c81ce84da5a91760c9d92cdb4180cdc119d6617
Manisha-Mohapatra <mmohapatra2307@gmail.com>
2025-05-06 19:57:23 +0530

    Delete Loyalty_Program directory

--- Loyalty_Program/src/main/java/com/sts/repository/Shopkeeper_SettingRepository.java
+++
@@ -1,9 +0,0 @@
-package com.sts.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.sts.entity.Shopkeeper_Setting;
-
-public interface Shopkeeper_SettingRepository extends JpaRepository<Shopkeeper_Setting, Long>{
-
-}
