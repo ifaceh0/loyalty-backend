73564f9742bd23f1a98231fbc70fa73cd4578526
Lappy <Lappy@HPPROBOOK.lan>
2025-05-06 11:46:24 +0530

    shop controller added

---
+++ Loyalty_Program/src/main/java/com/sts/controller/ShopController.java
@@ -0,0 +1,16 @@
+package com.sts.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.sts.entity.Shop;
+import com.sts.repository.ShopRepository;
+
+@RestController
+public class ShopController {
+	@Autowired
+	private ShopRepository shopRepository;
+	
+	
+}
