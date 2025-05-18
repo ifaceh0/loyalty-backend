58016e49c23cf4da730e61748d8376115cc006a6
sachin <sachin@gmail.com>
2025-05-18 17:51:11 +0530

    new frontend link added

--- Loyalty_Program/src/main/java/com/sts/configuration/WebConfig.java
+++ Loyalty_Program/src/main/java/com/sts/configuration/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer{
 	@Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://loyalty-frontend-mu.vercel.app", "http://localhost:5173")
+                .allowedOrigins("https://loyalty-frontend-gamma.vercel.app/", "http://localhost:5173")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
