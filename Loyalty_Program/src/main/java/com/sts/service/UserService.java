3c3a5d56e6409182ed166996ebca9ab93459654f
GIT <mmohapatra2307@gmail>
2025-05-20 21:36:52 +0530

    changes

--- Loyalty_Program/src/main/java/com/sts/service/UserService.java
+++ Loyalty_Program/src/main/java/com/sts/service/UserService.java
@@ -1,27 +1,15 @@
 package com.sts.service;
 
+import java.util.List;
 import java.util.Optional;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
 import com.sts.entity.User;
-import com.sts.repository.UserRepository;
 
-
-@Service
-public class UserService {
-	@Autowired
-	private UserRepository userService;
-	
-	
-
-	public User createUser(User user) {
-		
-		return userService.save(user);
-	}
-	
-//	public Optional<User> getUserById(Long id){
-//		return userService.findById(id);
-//	}
+public interface UserService 
+{
+	User createUser(User user);
+    List<User> getAllUsers();
+    Optional<User> getUserById(Long id);
+    User updateUser(Long id, User user);
+    void deleteUser(Long id);
 }
