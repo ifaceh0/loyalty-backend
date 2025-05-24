fd77c599e03742ebd7afdcc923319b9050a87f74
Suman <suman@gmail.com>
2025-05-24 16:48:45 +0530

    shopkeeper_setting change

--- Loyalty_Program/src/main/java/com/sts/entity/Shopkeeper_Setting.java
+++ Loyalty_Program/src/main/java/com/sts/entity/Shopkeeper_Setting.java
@@ -19,14 +19,28 @@ public class Shopkeeper_Setting {
 	    @JoinColumn(name = "shop_id")
 	    private Shop shop;
 
-	    private double dollarToPointMapping; 		//dollarPerPoint
+	    private double dollarToPointMapping;//dollarPerPoint
+	    private double sign_upBonuspoints;
 	    private double milestoneBonusAmount; 	//Coupon ($5) to redeem on next purchase
-
 	    private String specialBonusName;//Diwali bonus
 	    private double specialBonusPoints;//$ -> 10 points
 	    private LocalDate specialBonusStartDate;
 	    private LocalDate specialBonusEndDate;
-		public Long getId() {
+	    
+	    private String bonusdescription;	//Special Bonus Coupon Promotion (Begin Date, End Date, $ off) with description
+	    private LocalDate beginDate;
+	    private LocalDate endDate;
+	    private double amountOff;
+	    //constructor for special Bonus Coupon
+	    public Shopkeeper_Setting(String bonusdescription, LocalDate beginDate, LocalDate endDate, double amountOff) {
+	        this.bonusdescription = bonusdescription;
+	        this.beginDate= beginDate;
+	        this.endDate = endDate;
+	        this.amountOff = amountOff;
+	    }
+	    public Shopkeeper_Setting() {} 
+	    
+	    public Long getId() {
 			return id;
 		}
 		public void setId(Long id) {
