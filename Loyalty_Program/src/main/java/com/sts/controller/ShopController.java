ff88cdc67dfac06dfc59a4704a1261870cb5c2a9
GIT <mmohapatra2307@gmail>
2025-05-26 14:49:47 +0530

    changaes

--- Loyalty_Program/src/main/java/com/sts/controller/ShopController.java
+++ Loyalty_Program/src/main/java/com/sts/controller/ShopController.java
@@ -1,14 +1,22 @@
 package com.sts.controller;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.sts.entity.Shop;
+
+import com.sts.entity.UserProfile;
+import com.sts.repository.UserRepository;
 import com.sts.service.ShopService;
 
 import org.springframework.web.bind.annotation.GetMapping;
@@ -21,6 +29,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 public class ShopController {
 	@Autowired
 	private ShopService shopService;
+	
+	@Autowired
+	private UserRepository userRepository;
 	
 	
 	@PostMapping("/saveShop")
@@ -42,4 +53,83 @@ public class ShopController {
 				.orElseGet(() -> ResponseEntity.status(HttpStatus.NOT_FOUND)
 						.body(null));	
 	}
+	
+/*	//for Phonenumber fetching data
+	@GetMapping("/userInfo")
+	    public ResponseEntity<?> getUserByPhoneNumber(@RequestParam String phoneNumber) {
+	        Optional<User> userOpt = userRepository.findByPhoneNumber(phoneNumber);
+
+	        if (userOpt.isPresent()) {
+	            User user = userOpt.get();
+	            Map<String, String> userInfo = new HashMap<>();
+	            userInfo.put("firstName", user.getFirstName());
+	            userInfo.put("lastName", user.getLastName());
+	            userInfo.put("phoneNumber", user.getPhoneNumber());
+	            userInfo.put("email", user.getEmail());
+	            userInfo.put("totalPoints", profile != null ? profile.getTotalPoints() : 0);
+	            return ResponseEntity.ok(userInfo);
+	        } else {
+	            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+	                    .body("User not found with phone number: " + phoneNumber);
+	        }
+	    }
+	
+	
+	//For E-mail fetching data
+	@GetMapping("/userinfo-by-email")
+    public ResponseEntity<?> getUserByEmail(@RequestParam String email) {
+        Optional<User> userOpt = userRepository.findByEmail(email);
+
+        if (userOpt.isPresent()) {
+            User user = userOpt.get();
+            Map<String, String> userInfo = new HashMap<>();
+            userInfo.put("firstName", user.getFirstName());
+            userInfo.put("lastName", user.getLastName());
+            userInfo.put("phoneNumber", user.getPhoneNumber());
+            userInfo.put("email", user.getEmail());
+
+            return ResponseEntity.ok(userInfo);
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body("User not found with email: " + email);
+        }
+    }
+	*/
+	 @GetMapping("/userinfo-by-email")
+	    public ResponseEntity<?> getUserByEmail(@RequestParam String email) {
+	        return userRepository.findByEmail(email)
+	                .map(user -> {
+	                    UserProfile profile = user.getUserProfile();
+
+	                    Map<String, Object> userInfo = new HashMap<>();
+	                    userInfo.put("firstName", user.getFirstName());
+	                    userInfo.put("lastName", user.getLastName());
+	                    userInfo.put("email", user.getEmail());
+	                    userInfo.put("phoneNumber", user.getPhoneNumber());
+	                    userInfo.put("totalPoints", profile != null ? profile.getTotalPoints() : 0);
+
+	                    return ResponseEntity.ok(userInfo);
+	                })
+	                .orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).body(Map.of("error", "User not found")));
+	    }
+
+	 @GetMapping("/userinfo-by-phone")
+	 public ResponseEntity<Map<String, Object>> getUserByPhone(@RequestParam String phoneNumber) {
+	     return userRepository.findByPhoneNumber(phoneNumber)
+	             .map(user -> {
+	                 UserProfile profile = user.getUserProfile();
+
+	                 Map<String, Object> userInfo = new HashMap<>();
+	                 userInfo.put("firstName", user.getFirstName());
+	                 userInfo.put("lastName", user.getLastName());
+	                 userInfo.put("email", user.getEmail());
+	                 userInfo.put("phoneNumber", user.getPhoneNumber());
+	                 userInfo.put("totalPoints", profile != null ? profile.getTotalPoints() : 0);
+
+	                 return ResponseEntity.ok(userInfo);
+	             })
+	             .orElse(ResponseEntity.status(HttpStatus.NOT_FOUND)
+	                                   .body(Map.of("error", "User not found")));
+	 }
+
 }
--- Loyalty_Program/src/main/java/com/sts/repository/ShopRepository.java
+++ Loyalty_Program/src/main/java/com/sts/repository/ShopRepository.java
@@ -3,8 +3,13 @@ package com.sts.repository;
 
 
 
+
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.sts.entity.Shop;
 
-public interface ShopRepository extends JpaRepository<Shop, Long> {}
+public interface ShopRepository extends JpaRepository<Shop, Long>
+{
+	
+}
