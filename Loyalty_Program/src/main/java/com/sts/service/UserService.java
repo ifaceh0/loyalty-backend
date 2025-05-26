f8f5aed95f3a0cc5e0485c7740b6ef654daca05e
sachin <sachin@gmail.com>
2025-05-26 17:36:25 +0530

    user dto added

---
+++ Loyalty_Program/src/main/java/com/sts/service/UserService.java
@@ -0,0 +1,47 @@
+package com.sts.service;
+
+import com.sts.entity.User;
+import com.sts.repository.UserRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService 
+{
+    @Autowired
+    private UserRepository userRepository;
+
+    public User createUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public Optional<User> getUserById(Long id) {
+        return userRepository.findById(id);
+    }
+
+    public User updateUser(Long id, User userDetails) {
+        User user = userRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("User not found with id: " + id));
+
+        
+        user.setFirstName(userDetails.getFirstName());
+        user.setLastName(userDetails.getLastName());
+        user.setEmail(userDetails.getEmail());
+        user.setPhoneNumber(userDetails.getPhoneNumber());
+       
+        return userRepository.save(user);
+    }
+
+   /* @Override
+    public void deleteUser(Long id) {
+        userRepository.deleteById(id);
+    }*/
+}
