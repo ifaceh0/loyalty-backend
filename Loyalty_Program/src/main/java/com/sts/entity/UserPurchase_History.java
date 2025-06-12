bfbf3e4eb799797a0683b9ae3962e512701a6fab
Dharmapad13 <arakhasanta@gmail.com>
2025-06-12 20:27:21 +0530

    commit successful.

---
+++ Loyalty_Program/src/main/java/com/sts/entity/UserPurchase_History.java
@@ -0,0 +1,59 @@
+package com.sts.entity;
+
+import java.time.LocalDateTime;
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class UserPurchase_History 
+{
+	
+	@EmbeddedId
+    private UserPurchase_Id purchaseId;
+	
+	// Many purchase history records belong to one User
+    @ManyToOne
+    @JoinColumn(name = "user_id", insertable = false, updatable = false)
+    private User user;
+    // Many purchase history records belong to one Shop
+    @ManyToOne
+    @JoinColumn(name = "shop_id", insertable = false, updatable = false)
+    private Shop shop;
+
+    // Optional: timestamp of purchase
+    private LocalDateTime purchaseDate;
+
+    private Integer transactionAmount;
+
+    
+    // Getters and Setters 
+    
+	public UserPurchase_Id getPurchaseId() {
+		return purchaseId;
+	}
+
+	public void setPurchaseId(UserPurchase_Id purchaseId) {
+		this.purchaseId = purchaseId;
+	}
+
+	public LocalDateTime getPurchaseDate() {
+		return purchaseDate;
+	}
+
+	public void setPurchaseDate(LocalDateTime purchaseDate) {
+		this.purchaseDate = purchaseDate;
+	}
+
+	public Integer getTransactionAmount() {
+		return transactionAmount;
+	}
+
+	public void setTransactionAmount(Integer transactionAmount) {
+		this.transactionAmount = transactionAmount;
+	}
+}
---
+++ Loyalty_Program/src/main/java/com/sts/entity/UserPurchase_Id.java
@@ -0,0 +1,56 @@
+package com.sts.entity;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.IdClass;
+
+@Embeddable
+public class UserPurchase_Id implements Serializable
+{
+
+	    @Column(name = "user_id")
+	    private Long userId;
+
+	    @Column(name = "shop_id")
+	    private Long shopId;
+
+	    public UserPurchase_Id() {
+	    }
+
+	    public UserPurchase_Id(Long userId, Long shopId) {
+	        this.userId = userId;
+	        this.shopId = shopId;
+	    }
+
+	    public Long getUserId() {
+	        return userId;
+	    }
+
+	    public void setUserId(Long userId) {
+	        this.userId = userId;
+	    }
+
+	    public Long getShopId() {
+	        return shopId;
+	    }
+
+	    public void setShopId(Long shopId) {
+	        this.shopId = shopId;
+	    }
+	    
+	    @Override
+	    public boolean equals(Object o) {
+	        if (this == o) return true;
+	        if (!(o instanceof UserPurchase_Id)) return false;
+	        UserPurchase_Id that = (UserPurchase_Id) o;
+	        return Objects.equals(getUserId(), that.getUserId()) &&
+	               Objects.equals(getShopId(), that.getShopId());
+	    }
+	    @Override
+	    public int hashCode() {
+	        return Objects.hash(getUserId(), getShopId());
+	    }
+}
