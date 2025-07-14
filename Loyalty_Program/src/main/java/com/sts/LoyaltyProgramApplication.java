07a26dc8f925e73baf5ff0f00fdcad3760af2cf4
GIT <mmohapatra2307@gmail>
2025-07-14 08:51:40 +0530

    phone number and email

--- Loyalty_Program/src/main/java/com/sts/LoyaltyProgramApplication.java
+++ Loyalty_Program/src/main/java/com/sts/LoyaltyProgramApplication.java
@@ -6,6 +6,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 //@EnableTransactionManagement
+
 public class LoyaltyProgramApplication {
 	public static void main(String[] args){
 		SpringApplication.run(LoyaltyProgramApplication.class, args);
